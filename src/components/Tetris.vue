3f285306bdeb138dbc16bf9528b54b65b27980f8
Martin Horský <87446996+MartinH2k3@users.noreply.github.com>
2025-01-27 23:33:15 +0100

    defined basic shapes

--- src/components/Tetris.vue
+++ src/components/Tetris.vue
@@ -1,4 +1,84 @@
-<script setup>
+<script setup lang="ts">
+import {Point} from "@/utils/grid.js";
+
+abstract class Tetromino {
+  public tiles: Point[] = [];
+  protected constructor(public center: Point) {
+    this.center = center;
+  }
+  move(horizontally: number, vertically: number): void;
+  move(distance: Point): void;
+  move(a: number | Point, b?: number) {
+    if (typeof a === 'number')
+      this.tiles = this.tiles.map((point) => point.move(a, b as number));
+    else
+      this.tiles = this.tiles.map((point) => point.move(a));
+  }
+  rotate(): void {
+    this.tiles = this.tiles.map((point) => {
+      const relativeX = point.x - this.center.x;
+      const relativeY = point.y - this.center.y;
+      return this.center.move(-relativeY, relativeX);
+    });
+  }
+  rotate_back(): void {
+    this.tiles = this.tiles.map((point) => {
+      const relativeX = point.x - this.center.x;
+      const relativeY = point.y - this.center.y;
+      return this.center.move(relativeY, -relativeX);
+    });
+  }
+}
+
+class LBlock extends Tetromino {
+  constructor(public center: Point) {
+    super(center);
+    // center, upper part of L, lower part of L, right part of L
+    this.tiles = [center, center.move(0, -1), center.move(0, 1), center.move(1, 1)];
+  }
+}
+
+class JBlock extends Tetromino {
+  constructor(public center: Point) {
+    super(center);
+    // center, upper part of J, lower part of J, left part of J
+    this.tiles = [center, center.move(0, -1), center.move(0, 1), center.move(-1, 1)];
+  }
+}
+
+class ZBlock extends Tetromino {
+  constructor(public center: Point) {
+    super(center);
+    // center, upper part of Z, lower part of Z, right part of Z
+    this.tiles = [center, center.move(0, -1), center.move(1, 0), center.move(1, 1)];
+  }
+}
+
+class SBlock extends Tetromino {
+  constructor(public center: Point) {
+    super(center);
+    // center, upper part of S, lower part of S, left part of S
+    this.tiles = [center, center.move(0, -1), center.move(-1, 0), center.move(-1, 1)];
+  }
+}
+
+class TBlock extends Tetromino {
+  constructor(public center: Point) {
+    super(center);
+    // center, upper part of T, lower part of T, right part of T, left part of T
+    this.tiles = [center, center.move(0, -1), center.move(0, 1), center.move(1, 0), center.move(-1, 0)];
+  }
+}
+
+class IBlock extends Tetromino {
+  constructor(public center: Point) {
+    super(center);
+    this.tiles = [center, center.move(0, -1), center.move(0, 1)];
+  }
+  rotate() {
+
+  }
+}
 
 </script>
 
---
+++ src/utils/grid.ts
@@ -0,0 +1,31 @@
+export class Grid {
+  public grid: any[][];
+  constructor(public width: number, public height: number) {
+    this.width = width;
+    this.height = height;
+    this.grid = Array.from({ length: height }, () => Array(width).fill(null));
+  }
+  area() {
+    return this.width * this.height;
+  }
+}
+
+export class Point {
+  constructor(public x: number, public y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  move(horizontally: number, vertically: number): Point;
+  move(distance: Point): Point;
+  move(a: number | Point, b?: number): Point {
+    if (typeof a === 'number')
+      return new Point(this.x + a, this.y + (b as number));
+    else
+      return new Point(this.x + a.x, this.y + a.y);
+  }
+
+  within(grid: Grid) {
+    return this.x >= 0 && this.x < grid.width && this.y >= 0 && this.y < grid.height;
+  }
+}
